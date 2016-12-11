42980000b6748c221847b27f255aacc5d764c9e5
njj5210 <2311779138@qq.com>
2016-12-12 00:12:02 +0800

    55

---
+++ js/index.js
@@ -0,0 +1,41 @@
+window.onload=function() {
+    var myScroll;
+
+    function loaded() {
+        myScroll = new IScroll('#wrapper', {useTransition: false});
+    }
+
+    document.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    }, false);
+
+    loaded();
+
+
+
+    //设定iphone6的rem;100;
+    function resize(Origin,type){
+        var widths=document.documentElement.clientWidth;
+        var heights=document.documentElement.clientHeight;
+        var type=type||"x";
+        if(type=="x"){
+            var scale=widths/Origin*100;
+        }else if(type=="y"){
+            var scale=heights/Origin*100;
+        }
+        document.getElementsByTagName("html")[0].style.fontSize=scale+"px";
+    }
+    resize(750,"x");
+
+    function app(){
+        var swiper = new Swiper('.swiper-container', {
+            pagination : '.swiper-pagination',
+            paginationType : 'custom',
+            paginationCustomRender: function (swiper, current, total) {
+                return current + ' / ' + total;
+            }
+        });
+    }
+    app();
+
+}
